81cd7f7313aa21d93ab011c29c9351c98d651f1a
AIDAR <95232126+AidarTalgatbek@users.noreply.github.com>
2024-12-18 22:58:19 +0600

    Updated

--- app/views.py
+++ app/views.py
@@ -1,12 +1,11 @@
 import csv
 from django.http import HttpResponse
 import pandas as pd
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
+from sklearn.linear_model import LinearRegression
 from .models import Financial_Data, FinancialPlan, BudjetItem
 from .forms import FinancialPlanForm, BudjetItemForm, FinancialDataForm
 from django.db.models import Sum
@@ -37,38 +36,65 @@ class FinancialPlanDetailView(DetailView):
     model = FinancialPlan
     template_name = 'finance/plan_detail.html'
     context_object_name = 'plan'
+    form_class_budjet_item = BudjetItemForm
+    form_class_financial_data = FinancialDataForm
 
     def get_context_data(self, **kwargs):
-        """Добавляем экспортируемые данные в контекст."""
+        import numpy as np
+        import json
         context = super().get_context_data(**kwargs)
-        data = Financial_Data.objects.filter(plan_id=self.object.id)
-        context['financial_data'] = data
+        context['budjet_items'] = BudjetItem.objects.filter(plan=self.object)
+        context['financial_data'] = Financial_Data.objects.filter(plan=self.object)
+        context['budjet_item_form'] = kwargs.get('budjet_item_form', self.form_class_budjet_item())
+        context['financial_data_form'] = kwargs.get('financial_data_form', self.form_class_financial_data())
+
+        # Linear regression forecast
+        financial_data = context['financial_data']
+        if financial_data.count() > 1:
+            periods = np.array([i for i, _ in enumerate(financial_data)]).reshape(-1, 1)
+            values = np.array([data.actual_income_expence for data in financial_data]).reshape(-1, 1)
+            model = LinearRegression().fit(periods, values)
+            forecast = model.predict([[len(financial_data)]])[0][0]
+            context['forecast'] = f"{forecast:,.2f}".replace(',', ' ')
+
+            graph_labels = [data.period.strftime('%Y-%m-%d') for data in financial_data] + ["Прогноз"]
+            graph_values = [float(data.actual_income_expence) for data in financial_data] + [forecast]
+
+            # Передача графика в JSON-формате
+            context['graph_data'] = json.dumps({
+                "labels": graph_labels,
+                "values": graph_values
+            })
+        else:
+            context['forecast'] = "Недостаточно данных для прогноза"
+            context['graph_data'] = json.dumps({
+                "labels": [],
+                "values": []
+            })
         return context
     
     def post(self, request, *args, **kwargs):
         """Обработка добавления BudjetItem и Financial_Data."""
         self.object = self.get_object()
-
+        budjet_item_form = self.form_class_budjet_item(request.POST or None)
+        financial_data_form = self.form_class_financial_data(request.POST or None)
         # Обработка формы BudjetItem
-        budjet_form = BudjetItemForm(request.POST)
-        if budjet_form.is_valid():
-            budjet_item = budjet_form.save(commit=False)
-            budjet_item.plan = self.object
-            budjet_item.save()
-            return redirect('app:plan_detail', pk=self.object.pk)
+        if 'budjet_item_submit' in request.POST:
+            if budjet_item_form.is_valid():
+                budjet_item = budjet_item_form.save(commit=False)
+                budjet_item.plan = self.object
+                budjet_item.save()
+                return redirect(reverse('app:financial_plan_detail', kwargs={'pk': self.object.pk}))
 
-        # Обработка формы Financial_Data
-        financial_data_form = FinancialDataForm(request.POST)
-        if financial_data_form.is_valid():
-            financial_data = financial_data_form.save(commit=False)
-            financial_data.plan = self.object
-            financial_data.save()
-            return redirect('app:plan_detail', pk=self.object.pk)
+        if 'financial_data_submit' in request.POST:
+            if financial_data_form.is_valid():
+                financial_data = financial_data_form.save(commit=False)
+                financial_data.plan = self.object
+                financial_data.save()
+                return redirect(reverse('app:financial_plan_detail', kwargs={'pk': self.object.pk}))
 
         # Если формы невалидны, передаем их в контекст
-        context = self.get_context_data()
-        context['budjet_item_form'] = budjet_form
-        context['financial_data_form'] = financial_data_form
+        context = self.get_context_data(budjet_item_form=budjet_item_form, financial_data_form=financial_data_form)
         return self.render_to_response(context)
 
     def export_to_csv(self):
@@ -118,23 +144,6 @@ class FinancialPlanDetailView(DetailView):
 
         return response
     
-    def linear_regression_forecast(self, plan_id):
-        """Метод для прогноза финансовых данных с использованием линейной регрессии."""
-        data = list(Financial_Data.objects.filter(plan_id=plan_id, actual_income_expence__isnull=False)
-    .values_list('actual_income_expence', flat=True))
-        if len(data) < 2:
-            return None  # Недостаточно данных для прогноза.
-
-        # Построение модели линейной регрессии
-        x = np.arange(len(data)).reshape(-1, 1)
-        y = np.array(data)
-        model = LinearRegression()
-        model.fit(x, y)
-
-        # Прогноз на 12 периодов вперед
-        future = model.predict([[len(data) + i] for i in range(1, 13)])
-        return future.tolist()
-    
     def get(self, request, *args, **kwargs):
         """Обработка GET-запроса с экспортом."""
         if 'export' in request.GET:
@@ -147,35 +156,3 @@ class CreatePlanView(CreateView):
     form_class = FinancialPlanForm
     template_name = 'finance/create_plan.html'
     success_url = reverse_lazy('app:plan')
-    
-# def create_financial_plan(request):
-#     if request.method == "POST":
-#         form = FinancialPlanForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('main_page')  # перенаправление на список планов
-#     else:
-#         form = FinancialPlanForm()
-#     return render(request, 'finance/create_plan.html', {'form': form})
-
-
-# def add_budget_item(request):
-#     if request.method == "POST":
-#         form = BudjetItemForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('main_page')  # перенаправление на список планов
-#     else:
-#         form = BudjetItemForm()
-#     return render(request, 'finance/add_budget_item.html', {'form': form})
-
-
-# def add_financial_data(request):
-#     if request.method == "POST":
-#         form = Financial_DataForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('plan_list')
-#     else:
-#         form = Financial_DataForm()
-#     return render(request, 'finance/add_financial_data.html', {'form': form})
